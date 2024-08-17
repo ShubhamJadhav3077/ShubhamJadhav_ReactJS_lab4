d085ff5f2985bcdc4d1add32860cc0aa67489462
ShubhamJadhav3077 <108738143+ShubhamJadhav3077@users.noreply.github.com>
2024-08-17 13:48:46 +0530

    Add files via upload

---
+++ expense-tracker/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import ShowData from './Components/ShowData';
+import ExpenseTracker from './Components/ExpenseTracker';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ShowData></ShowData>}></Route>
+        <Route path="/add" element={<ExpenseTracker></ExpenseTracker>}></Route>
+      </Routes>
+      
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ expense-tracker/src/Services/menu.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+const getDataFromServer =()=>{
+    return axios.get('http://localhost:3002/items')
+    .then(response=>response.data)
+}
+const pushDataToServer =(newExpense)=>{
+    return axios.post("http://localhost:3002/items",newExpense,{
+        headers:{
+            'content-Type':"application/json"
+        }
+    }).then(response=>response.data)
+}
+export{
+    getDataFromServer,
+    pushDataToServer
+}
